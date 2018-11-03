f6d58f77b94d966deb8aa6fe77200b9d016be2e9
Evan Sturman <evan.sturman@gmail.com>
2018-11-03 20:49:16 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -30,7 +30,7 @@ def long_planeteer_calls(planeteer_calls)
   planeteer_calls.any? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(potentially_cheesy_items)
+
+  cheeses = %w[cheddar gouda camembert]
 end
